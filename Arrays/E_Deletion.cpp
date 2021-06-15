57986c41e8a303341dd4da7d4cb3ba88efcb9e08
Ahtisham <ahtishamshakir777@gmail.com>
2021-06-15 05:43:12 -0700

    Deletion from End

---
+++ Arrays/E_Deletion.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[10], size;
+    cout << "Enter the size of Array: ";
+    cin >> size;
+    if (size > 10 || size < 1)
+    {
+        cout << "You Entered Invalid size" << endl;
+        cout << "Size must be between 1 to 10" << endl;
+        exit(0);
+    }
+    else
+    {
+        for (int i = 0; i < size; i++)
+        {
+
+            cout << "Enter an integer: ";
+            cin >> arr[i];
+        }
+    }
+    cout << "Array before Deletion" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << "\t";
+    }
+    cout << endl;
+// Deletion From end
+    size--;
+    cout << "Array after Deletion from end" << endl;
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << "\t";
+    return 0;
+}
